c15d3997fbd56eab944d0526f86b5577b5a44e05
Eduardo Lenz <eduardobarplenz@gmail.com>
2024-03-12 17:35:57 -0300

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -29,5 +29,54 @@ end
     df = f(z).dual
 
     @test isapprox(df,0.0)
+
+end
+
+@testset "Multiplication" begin
+
+
+  z = LDual.Dual(2.0, 1.0)
+
+  # Tests the derivative 
+  f(x) = (x*2.0)+(3.0*x)+(x*x)
+
+  # Gets the derivative
+  df = f(z).dual
+
+  # Tests it
+  @test isapprox(df,(5+(2*z.real)))
+
+end
+
+
+@testset "Division" begin
     
+    z = LDual.Dual(2.0, 1.0)
+
+    # Tests the derivative
+    f(x) = (x/2.0)+(3.0/x)+(x/x)
+
+    # Gets the derivative
+    df = f(z).dual
+
+    # Tests it
+    @test isapprox(df,(0.5-(3/(z.real^2))))
+
+end
+
+
+@testset "Sine" begin
+    
+    # Initializes the input
+    z = LDual.Dual(2.0, 1.0)
+
+    # Tests the derivative 
+    f(x) = 3*sin(x)
+
+    # Gets the derivative
+    df = f(z).dual
+
+    # Tests it
+    @test isapprox(df,(3*cos(z.real)))
+
 end
